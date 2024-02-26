5bfb94db1619042db6d222b349d5dbfbcc8a0e29
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2024-02-26 22:44:12 +0000

    Add files via upload

---
+++ create_site.sh
@@ -0,0 +1,23 @@
+# Does the initial conversion of .md to .html
+# for i in *.md ; do echo "$i" && pandoc -s $i -o $i.html ; done
+for i in *.md ; do echo "$i" && pandoc -s "$i" -o "${i%.md}.html" ; done
+
+
+# Moves files to _sites directory.
+mkdir _site
+cd _site
+mkdir posts
+mkdir images
+mkdir css
+mkdir drafts
+cd ..
+
+mv about.html _site
+mv archive.html _site
+mv index.html _site
+mv extras.html _site
+
+cd _posts
+for i in *.md ; do echo "$i" && pandoc -s "$i" -o "${i%.md}.html" ; done
+
+mv 2024-02-31-welcome.html ..; cd ..; mv 2024-02-31-welcome.html _site/posts
